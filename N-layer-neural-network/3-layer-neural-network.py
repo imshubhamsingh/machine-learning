09894e5d858019712c6aed0664dacc1e11e5993d
Shubham Singh <imshubhamsingh97@gmail.com>
2017-07-27 16:13:36 +0530

    N layer neural layer network now working

--- N-layer-neural-network/3-layer-neural-network.py
+++ N-layer-neural-network/3-layer-neural-network.py
@@ -1,30 +1,42 @@
-
+#   XOR.py-A very simple neural network to do exclusive or.
 import numpy as np
 
-epochs = 60000
+epochs = 500  # Number of iterations
 inputLayerSize, hiddenLayerSize, outputLayerSize = 2, 3, 1
 
 X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 Y = np.array([[0], [1], [1], [0]])
 
 
-def sigmoid(x): return 1 / (1 + np.exp(-x))
+def sigmoid(x): return 1 / (1 + np.exp(-x))  # activation function
 
 
-def sigmoid_(x): return x * (1 - x)
+def sigmoid_(x): return x * (1 - x)  # derivative of sigmoid
 
 
 # weights on layer inputs
 Wh = np.random.uniform(size=(inputLayerSize, hiddenLayerSize))
 Wz = np.random.uniform(size=(hiddenLayerSize, outputLayerSize))
+print("Original output layer weights: \n", Wz)
+print("Original hidden layer weights: \n", Wh)
 
 for i in range(epochs):
     H = sigmoid(np.dot(X, Wh))  # hidden layer results
+    # print("Hidden", H)
     Z = sigmoid(np.dot(H, Wz))  # output layer results
+    # print("Output: \n", Z)
     E = Y - Z  # how much we missed (error)
+    # print("error: \n", E)
     dZ = E * sigmoid_(Z)  # delta Z
+    # print("dz", dZ)
+    # print("Wz.T", Wz.T)
     dH = dZ.dot(Wz.T) * sigmoid_(H)  # delta H
+    # print("dH", dH)
+    print("weight adjusted")
     Wz += H.T.dot(dZ)  # update output layer weights
+    # print("H.T.dot(dZ)", H.T.dot(dZ))
     Wh += X.T.dot(dH)  # update hidden layer weights
+    print("output layer weights: \n", Wz)
+    print("hidden layer weights: \n", Wh)
 
 print("Z: ", Z)  # what have we learnt?
--- N-layer-neural-network/demo.py
+++ N-layer-neural-network/demo.py
@@ -5,7 +5,6 @@ class NLayerNeuralNetwork:
     def __init__(self):
         self.layer = 1
         self.synaptic_weights = {}
-        self.adjustment = {}
         self.neurons_in_layers = {}
         self.hidden_layer_value = {}
         self.output_layer_value = {}
@@ -23,19 +22,17 @@ class NLayerNeuralNetwork:
 
     def output_layer_neuron(self, input):
         self.layer += 1
-        self.synaptic_weights[self.layer - 1] = 2 * random.random(
-            (self.neurons_in_layers[self.layer - 1], input)) - 1
-        # print(self.neurons_in_layers[self.layer])
-        self.adjustment[self.layer] = zeros((self.neurons_in_layers[self.layer - 1], input))
+        self.synaptic_weights[self.layer - 1] = random.uniform(
+            size=(self.neurons_in_layers[self.layer - 1], input))
+        # # print(self.neurons_in_layers[self.layer-1])
         self.neurons_in_layers[self.layer] = input
 
     def add_layer(self, no_of_sigmoid_neurons):
         self.layer += 1
         # adding random weight to new layer
-        self.synaptic_weights[self.layer - 1] = 2 * random.random(
-            (self.neurons_in_layers[self.layer - 1], no_of_sigmoid_neurons)) - 1
+        self.synaptic_weights[self.layer - 1] = random.uniform(
+            size=(self.neurons_in_layers[self.layer - 1], no_of_sigmoid_neurons))
         # setting initial adjustment to zero
-        self.adjustment[self.layer] = zeros((self.neurons_in_layers[self.layer - 1], no_of_sigmoid_neurons))
         self.neurons_in_layers[self.layer] = no_of_sigmoid_neurons
 
     def backward_propagation(self, training_set_inputs):
@@ -43,51 +40,48 @@ class NLayerNeuralNetwork:
             if layer != len(self.neurons_in_layers) and layer != 1:
                 self.delta[layer] = self.delta[layer + 1].dot(
                     self.synaptic_weights[layer].T) * self.__sigmoid_derivative(
-                    self.hidden_layer_value[layer - 1])
+                    self.hidden_layer_value[layer])
 
         for layer in sorted(self.neurons_in_layers.keys(), reverse=True):
-            if layer == len(self.neurons_in_layers):
-                self.synaptic_weights[layer - 1] += self.output_layer_value.T.dot(self.delta[layer])
-            elif layer < len(self.neurons_in_layers) and layer > 2:
+            if layer > 2:
                 self.synaptic_weights[layer - 1] += self.hidden_layer_value[layer - 1].T.dot(self.delta[layer])
-            elif layer == 1:
-                self.synaptic_weights[layer] += training_set_inputs.T.dot(self.delta[layer + 1])
+            elif layer == 2:
+                self.synaptic_weights[layer - 1] += training_set_inputs.T.dot(self.delta[layer])
             else:
                 pass
 
     def predict(self, inputs):
-        value = None
         for layer in self.neurons_in_layers:
+            # # print(layer)
             if layer != len(self.neurons_in_layers):
                 value = self.__sigmoid(dot(inputs, self.synaptic_weights[layer]))
                 inputs = value
                 if layer < len(self.neurons_in_layers) - 1:
-                    self.hidden_layer_value[layer] = value
+                    self.hidden_layer_value[layer + 1] = value
                 else:
                     self.output_layer_value = value
         return self.output_layer_value
 
-    def train(self, training_set_inputs, training_set_outputs, number_of_iterations=200000):
-        output = None
+    def train(self, training_set_inputs, training_set_outputs, number_of_iterations=6000):
         for iteration in range(number_of_iterations):
             output = self.predict(training_set_inputs)
             error_in_output = training_set_outputs - output
-            # print("error in output: ", error_in_output)
             self.delta[len(self.neurons_in_layers)] = error_in_output * self.__sigmoid_derivative(output)
             self.backward_propagation(training_set_inputs)
-        print("output: ", output)
 
 
 if __name__ == "__main__":
     neural_network = NLayerNeuralNetwork()
     neural_network.neurons_in_input_layer(2)
+    # hidden layer
+    neural_network.add_layer(3)
+    neural_network.add_layer(4)
     # output layer
     neural_network.output_layer_neuron(1)
-    # print(neural_network.synaptic_weights)
     training_set_input = array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    training_set_outputs = array([[1], [0], [0], [1]])
+    training_set_outputs = array([[0], [1], [1], [0]])
 
     neural_network.train(training_set_input, training_set_outputs)
-    # print('Predicting')
-    # print('Consider new situation [1, 0, 0] -> ?: ')
-    # print(neural_network.predict(array([1, 0, 0])))
+    print('Predicting')
+    print('Consider new situation [0, 0] -> ?: ')
+    print("Predicted Value: \n", neural_network.predict(array([[0, 0]])))
