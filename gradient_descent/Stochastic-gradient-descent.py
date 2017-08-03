8b4ad5c9ad67df78ab42fed34a58feebce321bb6
Shubham Singh <imshubhamsingh97@gmail.com>
2017-08-03 20:57:37 +0530

    stochastic gradient descent

--- gradient_descent/Stochastic-gradient-descent.py
+++ gradient_descent/Stochastic-gradient-descent.py
@@ -2,40 +2,40 @@ import numpy as np
 
 
 def evaluate_gradient(c_current, m_current, points, learning_rate):
-    c_gradient = 0
-    m_gradient = 0
-    n = float(len(points))
-    for i in range(0, len(points)):
-        x = points[i, 0]
-        y = points[i, 1]
-        c_gradient += -(2 / n) * (y - ((m_current * x) + c_current))
-        m_gradient += -(2 / n) * x * (y - ((m_current * x) + c_current))
-    new_c = c_current - (learning_rate * c_gradient)
-    new_m = m_current - (learning_rate * m_gradient)
-    return [new_c, new_m]
+    b_gradient = 0
+    a_gradient = 0
+    n = 1
+    x = points[0]
+    y = points[1]
+    b_gradient += -(2 / n) * (y - ((m_current * x * x) + c_current))
+    a_gradient += -(2 / n) * x * x * (y - ((m_current * x * x) + c_current))
+    new_b = c_current - (learning_rate * b_gradient)
+    new_a = m_current - (learning_rate * a_gradient)
+    return [new_b, new_a]
 
 
-def stochastic_gradient_descent(points, initial_c, initial_m, learning_rate, num_iteration):
-    c = initial_c
-    m = initial_m
+def stochastic_gradient_descent(points, params, learning_rate, num_iteration):
+    [b, a] = params
     for i in range(num_iteration):
         np.random.shuffle(points)
         for point in points:
-            [c, m] = evaluate_gradient(c, m, point, learning_rate)
-    return [c, m]
+            [b, a] = evaluate_gradient(b, a, point, learning_rate)
+    return [b, a]
 
 
 def run():
     x_data = np.linspace(-2, 2, 3000)[:, np.newaxis]
-    y_data = 0.3 * x_data + 0.4
-    points = np.array([x_data, y_data])
-    learning_rate = 0.0001  # hyperparameter tuning knobs for model
-    # y = mx + c
-    initial_c = 0
-    initial_m = 0
-    num_iteration = 1000
-    [c, m] = stochastic_gradient_descent(points, initial_c, initial_m, learning_rate, num_iteration)
-    print(c, m)
+    noise = np.random.normal(0, 0.05, x_data.shape)
+    y_data = 0.3 * np.square(x_data) + 0.4 + noise
+    points = np.concatenate((x_data, y_data), axis=1)
+    learning_rate = 0.05  # hyperparameter tuning knobs for model
+    # y = ax^2 + b
+    initial_b = 0
+    initial_a = 0
+    params = [initial_b, initial_a]
+    num_iteration = 2
+    [b, a] = stochastic_gradient_descent(points, params, learning_rate, num_iteration)
+    print(b, a)
 
 
 if __name__ == '__main__':
